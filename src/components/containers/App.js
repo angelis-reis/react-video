70319691f534b0d8b8299d845b431a6c00de1bcc
angelis-reis <angelisreisbiof@gmail.com>
2021-04-01 17:21:41 -0300

    pegando dados de cada vídeo pela API Youtube

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -4,6 +4,8 @@ import WbnPlayer from './WbnPlayer'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import GlobalStyle from '../styles/GlobalStyle'
 
+const playlistId = "PLXA_TifFgaBAu0l39GWyJVVr0azXpV9wz"
+
 
 function youtubeUrlMaker(videoId) {
 
@@ -19,97 +21,61 @@ function youtubeUrlMaker(videoId) {
 
 const App = () => {
 
-	const [youtubePlaylist, setYoutubePlaylist] = useState([])	
+	
 
+	const [youtubePlaylist, setYoutubePlaylist] = useState([])
 	const [videoIdList, setVideoIdList] = useState([])
-
 	const [videosInformations, setVideosInformations] = useState([])
 
-	useEffect(() => {  /* buscando a playlist de vídeos */		
+	
 
+	useEffect(() => {  /* buscando a playlist de vídeos */		
 		async function fetchPlaylist(){
 			try{
-				/* const request =  await axios.get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=id%2C%20contentDetails%2C%20status&maxResults=50&playlistId=PLAMoi5Bosmp0JjEmmRebON5Ai1MgS1qyf&key=AIzaSyCU6mKJFznpRYM-Qn5JIbNkjVqqPlflx4Q") */
-
-				const request =  await axios.get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=PLAMoi5Bosmp0JjEmmRebON5Ai1MgS1qyf&key=AIzaSyCU6mKJFznpRYM-Qn5JIbNkjVqqPlflx4Q")
-
+				const requestPlayList =  await axios.get(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=${playlistId}&key=AIzaSyCU6mKJFznpRYM-Qn5JIbNkjVqqPlflx4Q`)
 				/* setYoutubePlaylist(request.data.items.contentDetails.videoId) */
-
-				setYoutubePlaylist(request.data.items)
-				
-			} 
-			catch(err) {
+				setYoutubePlaylist(requestPlayList.data.items)				
+			} catch(err) {
 				console.log("Erro: ", err.response?.data?.error) 
 			}
 		}
 		fetchPlaylist()
-
-		
-
-		
-
 	}, [])
 
 	useEffect( () => {   /* criando um array com os videosId da playlist de vídeos */	
-
 		/* console.log("youtubePlaylist: ", youtubePlaylist) */
-
 		var video
 		var videoList =[]
-
 		youtubePlaylist.map((vid) =>{
-
 			video =  vid.contentDetails.videoId
 			videoList.push(video)
 		})
-
 		setVideoIdList(videoList)
 
-		
-
 	}, [youtubePlaylist])
-
 	console.log('Koca:videoIdList ', videoIdList);
 
 
 	useEffect( () => {   /* buscando informações de cada vídeo da lista */	
-
-
 		/* console.log("youtubePlaylist: ", youtubePlaylist) */
 
 		videoIdList.map((vidInfo) =>{
 
+			
 			async function fetchVideosInfo(){
-
-				try{					
-	
-					const request =  await axios.get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=PLAMoi5Bosmp0JjEmmRebON5Ai1MgS1qyf&key=AIzaSyCU6mKJFznpRYM-Qn5JIbNkjVqqPlflx4Q")
-	
+				try{
+					const requestVideoInfos =  await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20contentDetails%2C%20player&id=${vidInfo}&key=AIzaSyCU6mKJFznpRYM-Qn5JIbNkjVqqPlflx4Q`)
 					/* setYoutubePlaylist(request.data.items.contentDetails.videoId) */
-	
-					
-					
-				} 
-				catch(err) {
+					console.log('Koca: requestVideoInfos ', requestVideoInfos.data.items);
+				} catch(err) {
 					console.log("Erro: ", err.response?.data?.error) 
 				}
 			}
-			
 			fetchVideosInfo()
-
-
 		})
-
-		
-
-		
-
 	}, [videoIdList])
 
 	
-
-	
-
 	
 	
 	
